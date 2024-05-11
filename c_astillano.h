5282779b3ea58fef4e3a0dfafd71aea826ca1121
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2024-05-10 21:00:49 -0700

    Create c_astillano.h

---
+++ c_astillano.h
@@ -0,0 +1,39 @@
+#define AUTO auto
+#define ROMPER break 
+#define CASO case
+#define LETRA char
+
+#define CONST const
+#define CONTINUAR continue
+#define POR_DEFECTO default
+#define HACER do
+
+#define DOBLE double
+#define SI_NO else
+#define ENUM enum
+#define EXTERN extern
+
+#define FLOTANTE float
+#define POR for 
+#define IR_A goto
+#define SI if
+
+#define NUM2 int 
+#define NUM4 long
+#define REGISTRAR register
+#define DEVOLVER return
+
+#define NUM 1 short
+#define SIGNADO signed
+#define TAMANO_DE sizeof
+#define ESTATICO static
+
+#define ESTRUC struct
+#define DELTA switch 
+#define DEFINIR_TIPO typedef
+#define UNION union
+
+#define SIN_SIGNAR unsigned
+#define VACIO void
+#define VOLATIL volatile
+#define MIENTRAS while
